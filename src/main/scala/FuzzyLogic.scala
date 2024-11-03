c4a56f41fd21737664811be2edbb262af4e7289a
nrayan2 <nrayan2@uic.edu>
2024-11-03 15:29:40 -0600

    changes

--- src/main/scala/FuzzyLogic.scala
+++ src/main/scala/FuzzyLogic.scala
@@ -2,10 +2,18 @@
 object FuzzyLogic {
   case class FuzzyElement(name: String, membership: Double)
 
+  // fuzzy union between 2 fuzzy sets
+  // for each pair of elements from A and B, it takes the maximum membership value
+  // This is representative of the OR operation, getting the highest degree of truth
+
   def fuzzyUnion(setA: List[FuzzyElement], setB: List[FuzzyElement]): List[FuzzyElement] = {
     setA.zip(setB).map { case (a, b) => FuzzyElement(a.name, math.max(a.membership, b.membership)) }
   }
 
+
+  // fuzzy intersection between 2 fuzzy sets
+  // takes minimum value for each pair of elements
+  // Represents the AND operation
   def fuzzyIntersection(setA: List[FuzzyElement], setB: List[FuzzyElement]): List[FuzzyElement] = {
     setA.zip(setB).map { case (a, b) => FuzzyElement(a.name, math.min(a.membership, b.membership)) }
   }
@@ -14,6 +22,8 @@ object FuzzyLogic {
     set.map(e => FuzzyElement(e.name, 1 - e.membership))
   }
 
+  // fuzzy addition between 2 fuzzy sets
+  // adds membership values for each pair of elements
   def fuzzyAddition(setA: List[FuzzyElement], setB: List[FuzzyElement]): List[FuzzyElement] = {
     setA.zip(setB).map { case (a, b) => FuzzyElement(a.name, math.min(1, a.membership + b.membership)) }
   }
