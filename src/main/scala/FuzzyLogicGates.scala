7a4cb927bd6f890aebcf3e35d1b064c9ac9561f3
nrayan2 <nrayan2@uic.edu>
2024-11-03 12:59:43 -0600

    HW2 first commit changes

--- src/main/scala/FuzzyLogicGates.scala
+++ src/main/scala/FuzzyLogicGates.scala
@@ -4,9 +4,31 @@ import scala.collection.mutable
 // case class for fuzzy gates
 case class FuzzyGate(name: String, operation: List[Double] => Double)
 
+// class representation for HW2
+case class ClassDef(
+  name: String,
+  variables: mutable.Map[String, Any] = mutable.Map(),
+  methods: mutable.Map[String, MethodDef] = mutable.Map(),
+  parent: Option[ClassDef] = None
+) {
+  
+  def addVariable(varName: String, value: Any): Unit = {variables(varName) = value}
+  def addMethod(MethodName: String, method: MethodDef): Unit = {methods(MethodName) = method}
+  def getVariable(varName: String): Option[Any] = {variables.get(varName).orElse(parent.flatMap(_.getVariable(varName)))}
+  def InvokeMethod(methodName: String, params: Map[String, Any]): Any = {
+    methods.get(methodName)
+      .orElse(parent.flatMap(_.methods.get(methodName)))
+      .map(_.execute(params))
+      .getOrElse(throw new NoSuchElementException("Method can't be found"))
+  }
+}
+
+case class MethodDef(parameters: List[String], body: Map[String, Any] => Any) {def execute(params: Map[String, Any]): Any = body(params)}
+
 object FuzzyLogicGates {
   // map to store gates and operations
   private val gates = mutable.Map[String, FuzzyGate]()
+  private val classRegistry: mutable.Map[String, FuzzyGate] = mutable.Map()
 
   // helper to assign gate with its operation
   // allows user to define custom logic gates with behavior
@@ -19,4 +41,10 @@ object FuzzyLogicGates {
   def evaluateGate(gateName: String, inputs: List[Double]): Option[Double] = {
     GateOperations.evaluateGate(gateName, inputs)
   }
+
+  def defineClass(className: String, methodName: String, method: List[Double] =>Double): Unit =
+    {
+      classRegistry.get(className).foreach
+    } 
+
 }
